637b7323e9088ded48759c30c8448099f521f074
Daniel Rusnac <daniel.rusnac.personal@gmail.com>
2024-04-23 18:08:11 +0300

    Update ModelLoader.cs

--- BloomMare/Assets/BloomMare/Scripts/Lessons/ModelLoader.cs
+++ BloomMare/Assets/BloomMare/Scripts/Lessons/ModelLoader.cs
@@ -1,15 +1,12 @@
-using BloomMare.Data;
 using UnityEngine;
 
 namespace BloomMare.Lessons {
     public class ModelLoader : MonoBehaviour {
-        [SerializeField] private Lesson m_SelectedLesson;
-        [SerializeField] private GlobalConfig m_GlobalConfig;
         [SerializeField] private GameObject m_ScanOverlay;
 
         private void Start() {
-            var target = Instantiate(m_GlobalConfig.activeTarget);
-            var lessonModel = Instantiate(m_SelectedLesson.prefab, target.pivot.position, target.pivot.rotation, target.pivot);
+            var target = Instantiate(Global.config.activeTarget);
+            var lessonModel = Instantiate(Global.selectedLesson, target.pivot.position, target.pivot.rotation, target.pivot);
 
             target.TargetFound += OnTargetFound;
             target.TargetLost += OnTargetLost;
